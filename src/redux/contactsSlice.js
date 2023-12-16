fea5d9f39857cb39f15929c6039943b2e3084bb0
MoniekNK <monika.nowakowska01@gmail.com>
2023-12-17 00:40:54 +0100

    new

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,3 +1,4 @@
+import { createAction } from '@reduxjs/toolkit';
 export const addNewContact = createAction('contact/addContact');
 export const deleteContact = createAction('contact/deleteContact');
 export const setFilter = createAction('contact/setFilter');
